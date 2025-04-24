266f52cf36d2779f2c2e379f6b129b5892a6b489
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-24 23:40:48 +0600

    Copied a string

---
+++ StringCopy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char firstString[101], secondString[101];
+
+    scanf("%s %s", firstString, secondString);
+
+    int stringLength = strlen(secondString);
+
+    for (int i = 0; i <= stringLength; i++)
+    {
+        /* code */
+        firstString[i] = secondString[i];
+    }
+
+    printf("%s %s", firstString, secondString);
+
+    return 0;
+}
