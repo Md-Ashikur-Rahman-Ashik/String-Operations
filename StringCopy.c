eed57baddb4d46cc9aa34bd4df6cfb88fd2c1808
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-24 23:50:43 +0600

    Copied string with a built-in function

--- StringCopy.c
+++ StringCopy.c
@@ -1,3 +1,25 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     char firstString[101], secondString[101];
+
+//     scanf("%s %s", firstString, secondString);
+
+//     int stringLength = strlen(secondString);
+
+//     for (int i = 0; i <= stringLength; i++)
+//     {
+//         /* code */
+//         firstString[i] = secondString[i];
+//     }
+
+//     printf("%s %s", firstString, secondString);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 
@@ -7,13 +29,7 @@ int main()
 
     scanf("%s %s", firstString, secondString);
 
-    int stringLength = strlen(secondString);
-
-    for (int i = 0; i <= stringLength; i++)
-    {
-        /* code */
-        firstString[i] = secondString[i];
-    }
+    strcpy(firstString, secondString);
 
     printf("%s %s", firstString, secondString);
 
