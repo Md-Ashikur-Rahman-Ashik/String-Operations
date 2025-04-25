f4c5002d61f73f6ba511e031aaf64d4382f49464
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-25 09:42:11 +0600

    Explored string comparison

--- StringCompare.c
+++ StringCompare.c
@@ -1,10 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    int i = 0;
+
     char firstString[101], secondString[101];
 
     scanf("%s %s", firstString, secondString);
+
+    int firstStringLength = strlen(firstString);
+    int secondStringLength = strlen(secondString);
+
+    int stringLength;
+
+    if (firstStringLength > secondStringLength)
+    {
+        stringLength = firstStringLength;
+    }
+    else
+    {
+        stringLength = secondStringLength;
+    }
+
+    while (i < stringLength)
+    {
+        /* code */
+        if (firstString[i] == '\0' && secondString[i] == '\0')
+        {
+            /* code */
+            printf("Equal");
+            break;
+        }
+        else if (firstString[i] == '\0' || firstString[i] < secondString[i])
+        {
+            /* code */
+            printf("%s is smaller", firstString);
+            break;
+        }
+        else if (secondString[i] == '\0' || secondString[i] < firstString[i])
+        {
+            /* code */
+            printf("%s is smaller", secondString);
+            break;
+        }
+        else
+        {
+            i++;
+        }
+    }
 
     return 0;
 }
