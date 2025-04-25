7484d3193e77789a2c57978cc05ec3914c05038d
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-25 09:56:40 +0600

    Compared string using built-in function

--- StringCompare.c
+++ StringCompare.c
@@ -1,53 +1,81 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     int i = 0;
+
+//     char firstString[101], secondString[101];
+
+//     scanf("%s %s", firstString, secondString);
+
+//     int firstStringLength = strlen(firstString);
+//     int secondStringLength = strlen(secondString);
+
+//     int stringLength;
+
+//     if (firstStringLength > secondStringLength)
+//     {
+//         stringLength = firstStringLength;
+//     }
+//     else
+//     {
+//         stringLength = secondStringLength;
+//     }
+
+//     while (i < stringLength)
+//     {
+//         /* code */
+//         if (firstString[i] == '\0' && secondString[i] == '\0')
+//         {
+//             /* code */
+//             printf("Equal");
+//             break;
+//         }
+//         else if (firstString[i] == '\0' || firstString[i] < secondString[i])
+//         {
+//             /* code */
+//             printf("%s is smaller", firstString);
+//             break;
+//         }
+//         else if (secondString[i] == '\0' || secondString[i] < firstString[i])
+//         {
+//             /* code */
+//             printf("%s is smaller", secondString);
+//             break;
+//         }
+//         else
+//         {
+//             i++;
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-    int i = 0;
-
     char firstString[101], secondString[101];
 
     scanf("%s %s", firstString, secondString);
 
-    int firstStringLength = strlen(firstString);
-    int secondStringLength = strlen(secondString);
+    int compare = strcmp(firstString, secondString);
 
-    int stringLength;
-
-    if (firstStringLength > secondStringLength)
+    if (compare == 0)
     {
-        stringLength = firstStringLength;
+        /* code */
+        printf("Both strings are equal");
+    }
+    else if (compare == -1)
+    {
+        printf("%s is greater than %s", secondString, firstString);
     }
     else
     {
-        stringLength = secondStringLength;
-    }
-
-    while (i < stringLength)
-    {
-        /* code */
-        if (firstString[i] == '\0' && secondString[i] == '\0')
-        {
-            /* code */
-            printf("Equal");
-            break;
-        }
-        else if (firstString[i] == '\0' || firstString[i] < secondString[i])
-        {
-            /* code */
-            printf("%s is smaller", firstString);
-            break;
-        }
-        else if (secondString[i] == '\0' || secondString[i] < firstString[i])
-        {
-            /* code */
-            printf("%s is smaller", secondString);
-            break;
-        }
-        else
-        {
-            i++;
-        }
+        printf("%s is greater than %s", firstString, secondString);
     }
 
     return 0;
