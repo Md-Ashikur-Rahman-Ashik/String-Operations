09794fa3ff02f4526c94b0abcb84f123f4c0ea3c
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-25 07:44:09 +0600

    Adding two strings with built-in function

--- StringConcat.c
+++ StringConcat.c
@@ -1,3 +1,42 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     char firstString[101], secondString[101];
+
+//     scanf("%s %s", firstString, secondString);
+
+//     int firstStringLength = strlen(firstString);
+//     int secondStringLength = strlen(secondString);
+
+//     // char finalString[firstStringLength + secondStringLength];
+
+//     // for (int i = 0; i < firstStringLength; i++)
+//     // {
+//     //     /* code */
+//     //     finalString[i] = firstString[i];
+//     // }
+
+//     // for (int i = 0; i < secondStringLength; i++)
+//     // {
+//     //     /* code */
+//     //     finalString[i + firstStringLength] = secondString[i];
+//     // }
+
+//     // printf("%s %s", finalString, secondString);
+
+//     for (int i = 0; i <=  secondStringLength; i++)
+//     {
+//         /* code */
+//         firstString[i + firstStringLength] = secondString[i];
+//     }
+
+//     printf("%s %s", firstString, secondString);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 
@@ -7,30 +46,7 @@ int main()
 
     scanf("%s %s", firstString, secondString);
 
-    int firstStringLength = strlen(firstString);
-    int secondStringLength = strlen(secondString);
-
-    // char finalString[firstStringLength + secondStringLength];
-
-    // for (int i = 0; i < firstStringLength; i++)
-    // {
-    //     /* code */
-    //     finalString[i] = firstString[i];
-    // }
-
-    // for (int i = 0; i < secondStringLength; i++)
-    // {
-    //     /* code */
-    //     finalString[i + firstStringLength] = secondString[i];
-    // }
-
-    // printf("%s %s", finalString, secondString);
-
-    for (int i = 0; i <=  secondStringLength; i++)
-    {
-        /* code */
-        firstString[i + firstStringLength] = secondString[i];
-    }
+    strcat(firstString, secondString);
 
     printf("%s %s", firstString, secondString);
 
